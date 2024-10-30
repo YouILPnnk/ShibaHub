4d2b55cc407dcc66682448e76787bc34f6b0b2f4
Hoedfgsg <87256651+Hoedfgsg@users.noreply.github.com>
2024-10-30 22:49:59 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,2 +1,3 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoedfgsg/ShibaHub/main/KavoLib_fix.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("ShibaHub", "Synapse")
+local Tab = Window:NewTab("Main")
