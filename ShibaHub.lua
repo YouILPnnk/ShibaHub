4bf9ac93be0aeb629756738b989477dedbdb1dbc
Hoedfgsg <87256651+Hoedfgsg@users.noreply.github.com>
2024-10-30 23:12:11 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,9 +1,10 @@
+_G.AutoFarm = false
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
 local Window = Library.CreateLib("ShibaHub", "Synapse")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Main function")
-_G.AutoFarm = false
 Section:NewToggle("AutoFarm", "", function(state)
     if state then
         _G.AutoFarm = false
@@ -13,9 +14,10 @@ Section:NewToggle("AutoFarm", "", function(state)
 end)
 spawn(function()
    while task.wait() do
-       if _G.AutoFarm = false then
+        if _G.AutoFarm = false then
            print("A")
-       else
+        else
            print("B")
-   end
+        end
+    end
 end)
