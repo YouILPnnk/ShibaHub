b644091a2cdf8addf82fa5949c6c495732a32f23
Hoedfgsg <87256651+Hoedfgsg@users.noreply.github.com>
2024-10-30 22:02:11 +0700

    Update ShibaBuildABoatKAK.lua

--- ShibaBuildABoatKAK.lua
+++ ShibaBuildABoatKAK.lua
@@ -1,2 +1,1 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
