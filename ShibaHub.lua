a190aabef88c0fdf46641fb5a3e6f5fd8db98073
Hoedfgsg <87256651+Hoedfgsg@users.noreply.github.com>
2024-10-30 22:26:30 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,1 +1,14 @@
-print("ShibaHub_v0.1")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "ShibaHub", HidePremium = false, SaveConfig = true, ConfigFolder = "ShibaHub"})
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddToggle({
+	Name = "This is a toggle!",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
