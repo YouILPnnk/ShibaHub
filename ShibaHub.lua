db7e481facb82ba75cfdd32b04e2b19ac3d67f30
Hoedfgsg <87256651+Hoedfgsg@users.noreply.github.com>
2024-10-30 23:09:16 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -4,18 +4,18 @@ local Window = Library.CreateLib("ShibaHub", "Synapse")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Main function")
 _G.AutoFarm = false
- Section:NewToggle("AutoFarm", "AutoFarm is "..tostring(_G.AutoFarm), function(state)
+Section:NewToggle("AutoFarm", "", function(state)
     if state then
         _G.AutoFarm = false
     else
         _G.AutoFarm = true
     end
 end)
- spawn(function()
-    while task.wait() do
-        if _G.AutoFarm = false then
-            print("A")
-        else
-            print("B")
-    end
+spawn(function()
+   while task.wait() do
+       if _G.AutoFarm = false then
+           print("A")
+       else
+           print("B")
+   end
 end)
