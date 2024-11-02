8973bd8509bbcde64d0e50401d4ff379201216b9
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-11-02 22:02:50 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,1 +1,26 @@
+_G.AutoFarm = false
+_G.AutoBuyTool = false
 
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("ShibaHub : Build A Boat", "GrapeTheme")
+local Tab = Window:NewTab("General")
+local Section = Tab:NewSection("Main")
+Section:NewToggle("AutoFarm", "Version :  0", function(state)
+    if state then
+        _G.AutoFarm = true
+    else
+        _G.AutoFarm = false
+    end
+end)
+Section:NewToggle("AutoBuyTool", "Version :  0", function(state)
+    if state then
+        _G.AutoBuyTool = true
+    else
+        _G.AutoBuyTool = false
+    end
+end)
+for theme, color in pairs(themes) do
+    Section:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
