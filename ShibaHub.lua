a24544554d17aa3e572625cbb4672f626362e5e9
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-11-02 22:27:29 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,8 +1,17 @@
 _G.AutoFarm = false
 _G.AutoBuyTool = false
 
+local function MobileSupport()
+    for _,v in pairs(game.CoreGui:GetDescendants()) do
+        if v.Name == "Main" then
+            v.Active = true
+        end
+    end
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("ShibaHub : Build A Boat", "GrapeTheme")
+MobileSupport()
 local Tab = Window:NewTab("General")
 local Section = Tab:NewSection("Main")
 Section:NewToggle("AutoFarm", "Version :  0", function(state)
