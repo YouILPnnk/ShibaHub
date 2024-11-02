1f5911c6d0473eb95406d05eb5611564fb79f7e3
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-11-02 22:08:21 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,10 +1,18 @@
 _G.AutoFarm = false
 _G.AutoBuyTool = false
 
+local function MobileSupport()
+    local Main = game.CoreGui[LibName]:WaitForChild("Main")
+    Main.Active = true
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("ShibaHub : Build A Boat", "GrapeTheme")
+wait(1)
+MobileSupport()
 local Tab = Window:NewTab("General")
 local Section = Tab:NewSection("Main")
+local Section1 = Tab:NewSection("Tool")
 Section:NewToggle("AutoFarm", "Version :  0", function(state)
     if state then
         _G.AutoFarm = true
@@ -18,6 +26,9 @@ Section:NewToggle("AutoBuyTool", "Version :  0", function(state)
     else
         _G.AutoBuyTool = false
     end
+end)
+Section1:NewButton("1", "", function()
+    print("Clicked")
 end)
 for theme, color in pairs(themes) do
     Section:NewColorPicker(theme, "Change your "..theme, color, function(color3)
