3af9664e0ffd0788c4a426aa6826aa4d68ecd380
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-11-13 10:32:45 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,7 +1,5 @@
 local v1 = game.PlaceId
-local v2 = {
-    537413528
-}
+local v2 = 537413528
 if v1 == v2 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YouILPnnk/ShibaHub/main/"..v1..".lua"))()
 else
