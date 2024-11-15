b8743a1a32a1c5ca7691e5ab7b52b7e6d4a86c3a
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-11-15 21:48:52 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -1,7 +1,7 @@
 local v1 = game.PlaceId
 local v2 = 537413528
 if v1 == v2 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/YouILPnnk/ShibaHub/main/"..v1..".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/YouILPnnk/Map/main/"..v1..".lua"))()
 else
     game.Players.LocalPlayer:Kick("ไม่พบแมพ")
 end
