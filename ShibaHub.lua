506c5d5c0f67dd43f31d179a56c48dd36f292e4a
YouILPnnk <87256651+YouILPnnk@users.noreply.github.com>
2024-12-17 18:08:18 +0700

    Update ShibaHub.lua

--- ShibaHub.lua
+++ ShibaHub.lua
@@ -2,5 +2,5 @@ local v1 = game.PlaceId
 if v1 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YouILPnnk/Map/main/"..v1..".lua"))()
 else
-    game.Players.LocalPlayer:Kick("ไม่พบแมพ")
+    game.Players.LocalPlayer:Kick("[MFCS]")
 end
